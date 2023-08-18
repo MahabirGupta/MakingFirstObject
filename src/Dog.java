74325a23e446c6f779cfb824502c583d9151bcaa
MahabirGupta <mahabirg81@gmail.com>
2023-08-18 13:44:58 +0800

    Create Dog class and object of Dog class to set size and call method

---
+++ src/Dog.java
@@ -0,0 +1,12 @@
+public class Dog {
+
+//    Setting the instance variables
+    int size;
+    String breed;
+    String name;
+
+//    Create a bark() method
+    void bark(){
+        System.out.println("Woof! Woof");
+    }
+}
---
+++ src/DogTestDrive.java
@@ -0,0 +1,8 @@
+public class DogTestDrive {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog(); // making a dog object
+        dog.size = 40; //use the dot operator(.) to set the size of the dog
+        dog.bark(); //use the dot operator(.) to call its bark() method
+    }
+}
